6746d93c3c0db95f4444f082f8531064dde8c720
ryan <ryansen87@gmail.com>
2025-02-21 01:37:09 -0500

    tanh and graph rendering

--- engine.py
+++ engine.py
@@ -1,8 +1,11 @@
+from graphviz_util import render
+import math
 class Val:
     def __init__(self, value, children=(), op=""):
         self.value = value
         self.children = children
         self.op = op
+        self.grad = 0
 
     def __add__(self, other):
         return Val(self.value + other.value, (self,other), "+")
@@ -10,12 +13,37 @@ class Val:
     def __mul__(self, other):
         return Val(self.value * other.value, (self,other), "*")
 
+    def tanh(self):
+        x = self.value
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Val(t, (self,), 'tanh')
+        return out
+
+    def backward(self):
+        self.grad = 1
     def __repr__(self):
         return f"Val({self.value})"
 
 
-a = Val(2)
-b = Val(5)
-c = Val(12)
+# inputs x1,x2
+x1 = Val(2.0)
+x2 = Val(0.0)
 
-print(a * b + c)
+# weights w1,w2
+w1 = Val(-3.0)
+w2 = Val(1.0)
+
+# bias of the neuron
+b = Val(6.8813735870195432)
+
+# x1*w1 + x2*w2 + b
+x1w1 = x1*w1
+x2w2 = x2*w2
+
+x1w1x2w2 = x1w1 + x2w2
+
+n = x1w1x2w2 + b
+
+o = n.tanh()
+
+render(o)
--- graphviz_util.py
+++ graphviz_util.py
@@ -7,7 +7,7 @@ def trace(root):
     def build(v):
         if v not in nodes:
             nodes.add(v)
-            for child in v._prev:
+            for child in v.children:
                 edges.add((child, v))
                 build(child)
 
@@ -22,15 +22,18 @@ def draw_dot(root):
     for n in nodes:
         uid = str(id(n))
         # for any value in the graph, create a rectangular ('record') node for it
-        dot.node(name=uid, label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
-        if n._op:
+        dot.node(name=uid, label="{ value %.4f | grad %.4f }" % (n.value, n.grad), shape='record')
+        if n.op:
             # if this value is a result of some operation, create an op node for it
-            dot.node(name=uid + n._op, label=n._op)
+            dot.node(name=uid + n.op, label=n.op)
             # and connect this node to it
-            dot.edge(uid + n._op, uid)
+            dot.edge(uid + n.op, uid)
 
     for n1, n2 in edges:
         # connect n1 to the op node of n2
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+        dot.edge(str(id(n1)), str(id(n2)) + n2.op)
 
     return dot
+
+def render(root):
+    draw_dot(root).render('graph', view=False, cleanup=True)
