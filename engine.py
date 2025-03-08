900e5d2d19a7fbc85cffe1b5e44ff651911941e2
ryan <ryansen87@gmail.com>
2025-03-07 19:39:25 -0500

    backward topo

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict, deque
 class Val:
     def __init__(self, value, children=(), op="", label=""):
         self.value = value
@@ -6,6 +7,26 @@ class Val:
         self.op = op
         self.grad = 1
         self.label = label
+
+        if self.op == "*":
+            def _backward():
+                self.children[0].grad += self.children[1].value * self.grad
+                self.children[1].grad += self.children[0].value * self.grad
+
+            self._backward = _backward
+
+        elif self.op == "/":
+            def _backward():
+                self.children[0].grad += 1 / self.children[1].value * self.grad
+                self.children[1].grad += (-self.children[0].value / (self.children[1].value * self.children[1].value)) * self.grad
+
+            self._backward = _backward
+
+        elif self.op == "tanh":
+            def _backward():
+                self.children[0].grad += (1 - (self.value * self.value)) * self.grad
+
+            self._backward = _backward
 
     def __add__(self, other):
         return Val(self.value + other.value, (self,other), "+")
@@ -27,25 +48,43 @@ class Val:
         t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
         return Val(t, (self,), 'tanh')
 
+    def _backward(self):
+        return None
     def backward(self):
-        if self.op == "":
-            return None
+        self.grad = 1
 
-        if self.op == "*":
-            self.children[0].grad = self.children[1].value
-            self.children[1].grad = self.children[0].value
+        topo = self.topological_order()
 
-        if self.op == "/":
-            self.children[0].grad = 1/self.children[1].value
-            self.children[1].grad = -self.children[0].value/(self.children[1].value*self.children[1].value)
+        for node in topo:
+            node._backward()
 
-        if self.op == "tanh":
-            self.children[0].grad = 1 - (self.value * self.value)
+    def topological_order(self):
+        in_edges = defaultdict(int)
+        visited = set()
+        def visit(node):
+            for child in node.children:
+                in_edges[child] += 1
+                if child not in visited:
+                    visited.add(child)
+                    visit(child)
 
-        for child in self.children:
-            child.grad *= self.grad
-            child.backward()
+        queue = deque([])
+        for node in in_edges:
+            if in_edges[node] == 0:
+                queue.append(node)
 
+        result = []
+        while len(queue):
+            curr = queue.popleft()
+            result.append(curr)
+
+            for child in curr.children:
+                in_edges[child] -= 1
+
+                if in_edges[child] == 0:
+                    queue.append(child)
+
+        return result
 
 
     def __repr__(self):
